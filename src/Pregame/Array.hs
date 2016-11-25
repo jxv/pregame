3fff75a38aeb2d9762670ee4d3bbe3b82117ed3e
jxv <joevargas92@gmail.com>
2016-11-25 14:02:40 -0800

    more array

--- src/Pregame/Array.hs
+++ src/Pregame/Array.hs
@@ -5,3 +5,25 @@ module Pregame.Array
 import Data.Array as X
   ( Array
   )
+import Data.Array.IArray as X
+  ( IArray
+  )
+import Data.Array.IO as X
+  ( IOArray
+  , IOUArray
+  , hGetArray
+  , hPutArray
+  )
+import Data.Array.ST as X
+  ( STArray
+  , STUArray
+  )
+import Data.Array.MArray as X
+  ( MArray
+  )
+import Data.Array.Storable as X
+  ( StorableArray
+  )
+import Data.Array.Unboxed as X
+  ( UArray
+  )
